3b5c6671f244896a12166950fe7f73a7f9606720
dara.dadachanji <dara.dadachanji@irhythmtech.com>
2023-10-27 17:16:19 -0500

    added delimiter option

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -10,8 +11,16 @@ import (
 )
 
 func main() {
+	delimiter := flag.String(
+		"d",
+		" ",
+		"delimiter for outputs if multiple capture groups are specified",
+	)
+	flag.Parse()
+	fmt.Println(*delimiter)
+
 	reader := bufio.NewReader(os.Stdin)
-	pattern := parsePatternArgument()
+	pattern := parsePatternArgument(flag.Args())
 	rex := regexp.MustCompile(pattern)
 	for {
 		line, readErr := reader.ReadString('\n')
@@ -23,7 +32,7 @@ func main() {
 			}
 			continue
 		}
-		output := strings.Join(captures[1:], " ")
+		output := strings.Join(captures[1:], *delimiter)
 		fmt.Print(output)
 		if readErr == io.EOF {
 			return
@@ -34,14 +43,18 @@ func main() {
 }
 
 // parsePatternArgument parses the regex pattern from the command line arguments
-func parsePatternArgument() string {
-	if len(os.Args) > 2 {
+func parsePatternArgument(args []string) string {
+	if len(args) > 1 {
 		_, _ = fmt.Fprintf(os.Stderr, "too many arguments")
+		for _, arg := range args {
+			_, _ = fmt.Fprintf(os.Stderr, "\n")
+			_, _ = fmt.Fprintf(os.Stderr, arg)
+		}
 		os.Exit(1)
 	}
-	if len(os.Args) < 2 {
+	if len(args) < 1 {
 		_, _ = fmt.Fprintf(os.Stderr, "missing pattern")
 		os.Exit(1)
 	}
-	return os.Args[1]
+	return args[0]
 }
